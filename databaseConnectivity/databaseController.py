09c2d573d5c8d7a8fc169f925485df4fd70977de
ZerolinK <david.e.vizcaino@gmail.com>
2014-11-27 23:06:52 -0500

    Added new databaseController
    Added the new database controller written by Danny

--- databaseConnectivity/databaseController.py
+++ databaseConnectivity/databaseController.py
@@ -14,12 +14,12 @@ class DatabaseController(object):
                 self.cursor.execute(query)
                 self.connection.commit()
 
-        def get_pass(self, email):
-                query= "SELECT Pass FROM user WHERE Email = '" +email+"'"
+        def verify_account(self, email, user_password):
+                query = "SELECT Pass FROM user WHERE Email = '" + email +"'"
                 self.cursor.execute(query)
                 fetch = self.cursor.fetchone()
                 password = " ".join(map(str, fetch))
-                return password
+                return check_password_hash(password, user_password)
 
         def set_report(self, reportID, userID, summary, description):
                 query = "INSERT INTO `testdb`.`report` (`Report_ID`, `User_ID`, `Summary`, `Description`, `Votes`, `Is_Resolved`) VALUES ('" + reportID + "', '" + userID + "', '" + summary + "', '" + description + "', '0', '0')"
@@ -58,10 +58,22 @@ class DatabaseController(object):
                 report = " ".join(map(str, fetch))
                 return report
 
+        def create_basic_user(self, userID, fName, lName, email, password):
+                password2 = generate_password_hash(password)
+                query = "INSERT INTO `testdb`.`user` (`ID`, `FName`, `LName`, `Email`, `Pass`, `Role`) VALUES ('" + userID + "', '" + fName + "', '" + lName + "', '" + email +"', '" + password2 + "', '0')"
+                self.cursor.execute(query)
+                self.connection.commit()
+
+        def create_faculty_user(self, userID, fName, lName, email, password):
+                password2 = generate_password_hash(password)
+                query = "INSERT INTO `testdb`.`user` (`ID`, `FName`, `LName`, `Email`, `Pass`, `Role`) VALUES ('" + userID + "', '" + fName + "', '" + lName + "', '" + email +"', '" + password2 + "', '1')"
+                self.cursor.execute(query)
+                self.connection.commit()
+        
         def close_connection(self):
                 self.connection.close()
         
-#dbc = DatabaseController('localhost', 3306, 'testuser', 'test623', 'testdb')
-#print(dbc.get_pass("dgonz023@fiu.edu"))
-#print(dbc.get_report("2"))
-#print(dbc.get_vote("2"))
+dbc = DatabaseController('localhost', 3306, 'testuser', 'test623', 'testdb')
+dbc.create_basic_user("1586390", "Daniel", "Gonzalez", "dgonz023@fiu.edu", "dpnet")
+print(dbc.verify_account("dgonz023@fiu.edu", "dpnet"))
+
