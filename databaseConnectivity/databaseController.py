3369ddfdb0590292537c3ac375252771c7bb287e
ZerolinK <david.e.vizcaino@gmail.com>
2014-11-28 00:39:29 -0500

    Modification of SQL queries
    Modified SQL queries to protect against injection

--- databaseConnectivity/databaseController.py
+++ databaseConnectivity/databaseController.py
@@ -5,54 +5,61 @@ class DatabaseController(object):
         def __init__(self, connection_address, connection_port, user_name, password, database):
                 self.connection = mysql.connector.connect(host = connection_address, user=user_name, password = password, db = database)
                 self.cursor = self.connection.cursor(buffered=True)
-        def create_junk_table(self):
+        def create_table(self):
                 query = "CREATE TABLE IF NOT EXISTS DPNET(why_mySQL int)"
                 self.cursor.execute(query)
                 self.connection.commit()
-        def destroy_junk_table(self):
+        def destroy_table(self):
                 query = "DROP TABLE IF EXISTS DPNET"
                 self.cursor.execute(query)
                 self.connection.commit()
 
         def verify_account(self, email, user_password):
-                query = "SELECT Pass FROM user WHERE Email = '" + email +"'"
-                self.cursor.execute(query)
+                #query = "SELECT Pass FROM user WHERE Email = '" + email +"'"
+                query = "SELECT Pass FROM user WHERE Email = %s"
+                self.cursor.execute(query, (email, ))
                 fetch = self.cursor.fetchone()
                 password = " ".join(map(str, fetch))
                 return check_password_hash(password, user_password)
 
         def set_report(self, reportID, userID, summary, description):
-                query = "INSERT INTO `testdb`.`report` (`Report_ID`, `User_ID`, `Summary`, `Description`, `Votes`, `Is_Resolved`) VALUES ('" + reportID + "', '" + userID + "', '" + summary + "', '" + description + "', '0', '0')"
-                self.cursor.execute(query)
+                #query = "INSERT INTO `testdb`.`report` (`Report_ID`, `User_ID`, `Summary`, `Description`, `Votes`, `Is_Resolved`) VALUES ('" + reportID + "', '" + userID + "', '" + summary + "', '" + description + "', '0', '0')"
+                query = "INSERT INTO `testdb`.`report` (`Report_ID`, `User_ID`, `Summary`, `Description`, `Votes`, `Is_Resolved`) VALUES (%S, %s, %s, %s, '0', '0')"
+                self.cursor.execute(query, (reportID, userID, summary, description))
                 self.connection.commit()
 
         def increment_vote(self, reportID):
-                query1 = "SELECT Votes FROM report WHERE Report_ID = '" + reportID +"'"
-                self.cursor.execute(query1)
+                #query1 = "SELECT Votes FROM report WHERE Report_ID = '" + reportID +"'"
+                query1 = "SELECT Votes FROM report WHERE Report_ID = %s"
+                self.cursor.execute(query1, (reportID, ))
                 fetch = self.cursor.fetchone()
                 curVote = " ".join(map(str, fetch))
                 intVote = int(curVote)
                 intVote = intVote + 1
-                query2 = "UPDATE `testdb`.`report` SET `Votes` = '" + str(intVote) + "' WHERE `report`.`Report_ID` = " + reportID
-                self.cursor.execute(query2)
+                #query2 = "UPDATE `testdb`.`report` SET `Votes` = '" + str(intVote) + "' WHERE `report`.`Report_ID` = " + reportID
+                query2 = "UPDATE `testdb`.`report` SET `Votes` = '" + str(intVote) + "' WHERE `report`.`Report_ID` = %s"
+                self.cursor.execute(query2, (reportID, ))
                 self.connection.commit()
 
         def get_vote(self, reportID):
-                query1 = "SELECT Votes FROM report WHERE Report_ID = '" + reportID +"'"
-                self.cursor.execute(query1)
+                #query1 = "SELECT Votes FROM report WHERE Report_ID = '" + reportID +"'"
+                query1 = "SELECT Votes FROM report WHERE Report_ID = %s"
+                self.cursor.execute(query1, (reportID, ))
                 fetch = self.cursor.fetchone()
                 curVote = " ".join(map(str, fetch))
                 intVote = int(curVote)
                 return intVote
 
         def resolve_issue(self, reportID):
-                query = "UPDATE `testdb`.`report` SET `Is_Resolved` = '1' WHERE `report`.`Report_ID` = " + reportID
-                self.cursor.execute(query)
+                #query = "UPDATE `testdb`.`report` SET `Is_Resolved` = '1' WHERE `report`.`Report_ID` = " + reportID
+                query = "UPDATE `testdb`.`report` SET `Is_Resolved` = '1' WHERE `report`.`Report_ID` = %s"
+                self.cursor.execute(query, (reportID, ))
                 self.connection.commit()
 
         def get_report(self, reportID):
-                query = "SELECT * FROM report WHERE Report_ID = " + reportID 
-                self.cursor.execute(query)
+                #query = "SELECT * FROM report WHERE Report_ID = " + reportID 
+                query = "SELECT * FROM report WHERE Report_ID = %s"
+                self.cursor.execute(query, (reportID, ))
                 self.connection.commit()
                 fetch = self.cursor.fetchone()
                 report = " ".join(map(str, fetch))
@@ -60,20 +67,31 @@ class DatabaseController(object):
 
         def create_basic_user(self, userID, fName, lName, email, password):
                 password2 = generate_password_hash(password)
-                query = "INSERT INTO `testdb`.`user` (`ID`, `FName`, `LName`, `Email`, `Pass`, `Role`) VALUES ('" + userID + "', '" + fName + "', '" + lName + "', '" + email +"', '" + password2 + "', '0')"
-                self.cursor.execute(query)
+                #query = "INSERT INTO `testdb`.`user` (`ID`, `FName`, `LName`, `Email`, `Pass`, `Role`) VALUES ('" + userID + "', '" + fName + "', '" + lName + "', '" + email +"', '" + password2 + "', '0')"
+                query = "INSERT INTO `testdb`.`user` (`ID`, `FName`, `LName`, `Email`, `Pass`, `Role`) VALUES (%s, %s, %s, %s, %s, '0')"
+                self.cursor.execute(query, (userID, fName, lName, email, password2))
                 self.connection.commit()
+
+        def get_basic_user(self, userMail):
+                #query = "SELECT * FROM user WHERE EMail = " + userMail
+                query = "SELECT * FROM user WHERE EMail = %s"
+                self.cursor.execute(query, (userMail, ))
+                self.connection.commit()
+                fetch = self.cursor.fetchone()
+                userData = " ".join(map(str, fetch))
+                return userData
 
         def create_faculty_user(self, userID, fName, lName, email, password):
                 password2 = generate_password_hash(password)
-                query = "INSERT INTO `testdb`.`user` (`ID`, `FName`, `LName`, `Email`, `Pass`, `Role`) VALUES ('" + userID + "', '" + fName + "', '" + lName + "', '" + email +"', '" + password2 + "', '1')"
-                self.cursor.execute(query)
+                #query = "INSERT INTO `testdb`.`user` (`ID`, `FName`, `LName`, `Email`, `Pass`, `Role`) VALUES ('" + userID + "', '" + fName + "', '" + lName + "', '" + email +"', '" + password2 + "', '1')"
+                query = "INSERT INTO `testdb`.`user` (`ID`, `FName`, `LName`, `Email`, `Pass`, `Role`) VALUES (%s, %s, %s, %s, %S, '1')"
+                self.cursor.execute(query, (userID, fName, lName, email, password2))
                 self.connection.commit()
         
         def close_connection(self):
                 self.connection.close()
         
-dbc = DatabaseController('localhost', 3306, 'testuser', 'test623', 'testdb')
-dbc.create_basic_user("1586390", "Daniel", "Gonzalez", "dgonz023@fiu.edu", "dpnet")
-print(dbc.verify_account("dgonz023@fiu.edu", "dpnet"))
+#dbc = DatabaseController('localhost', 3306, 'testuser', 'test623', 'testdb')
+#dbc.create_basic_user("1586390", "Daniel", "Gonzalez", "dgonz023@fiu.edu", "dpnet")
+#print(dbc.verify_account("dgonz023@fiu.edu", "dpnet"))
 
--- server/mainController.py
+++ server/mainController.py
@@ -3,10 +3,9 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-
+from databaseConnectivity import databaseController
 from tornado.options import define, options
 define("port", default=8000, help="run on specified port", type=int)
-
 
 class BaseController(tornado.web.RequestHandler):
     def get_current_user(self):
@@ -23,14 +22,21 @@ class IndexController(tornado.web.RequestHandler):
     #TODO: define this shit
 
 class LoginController(BaseController):
-    def get(self):
-        self.render(login.html)
     def post(self):
-        self.set_secure_cookie("username", self.get_argument("username"))#passed from html with the tag username
+        #tries = 0
+        #while (tries < 3)
+        if (databaseControl.verify_account(self.get_argument("usermail"), self.get_argument("password"))):
+            #databaseControl.
+            self.set_secure_cookie("username", self.get_argument("usermail"))#passed from html with the tag username
+            self.redirect("/", permanent=True)
+        '''TODO: return message stating password is incorrect and to try again'''
         #if PASSWORD is good, self.set_secure_cookie(username, self.get_argument("username"))
         self.redirect("/", permanent=True)#if permanent = true, when user refreshes, more form data will NOT be sent
     def get(self):
         self.render('login.html')#login.html page to be rendered
+    '''def put(self):
+        RETRIVE AND STORE USER DATA IN DATABASE'''
+
 
     class LogoutController(BaseController):
         def get(self):
@@ -50,8 +56,6 @@ class ReportController(BaseController):
 	    #def post(self):
 	    #	variable_1 = self.get_argument("form variable name here")
 	    #	....
-	    #	if PASSWORD is good, self.set_secure_cookie(username, self.get_argument("username"))'''
-
 	        #put appropriate fetches from template here and send to database
 
 '''class UserProfileController(BaseController):'''
@@ -61,15 +65,19 @@ def launch():
     server_settings = {"static_path": os.path.join(os.path.dirname(__file__), "./static"), 
     "template_path": "./server/templates", 
     "login_url": "/login", 
-    "cookie_secret": os.urandom(24), 
-    "xsrf_cookies": True}
+    "cookie_secret": os.urandom(24)}
 
     handlers = [ (r'/', IndexController),
         (r'/report', ReportController),
         (r'/create', ReportController.NewReportController), 
         (r'/login', LoginController), 
         (r'/logout', LoginController.LogoutController) ]
-    
+
+    global databaseControl
+    databaseControl = databaseController.DatabaseController('localhost', 3306, 'testuser', 'test623', 'testdb')
+
+    #databaseControl.create_basic_user("6003091", "David", "Vizcaino", "dvizc001@fiu.edu", "dpnet")
+
     application = tornado.web.Application(handlers, **server_settings)
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
